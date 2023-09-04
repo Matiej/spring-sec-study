8e6b2d83db826e93b4fd72e8f269be33cc9ab656
Maciej Wójcik <maciek.wojcik179@gmail.com>
2023-09-04 22:53:40 +0200

    add new security test template

---
+++ src/main/java/com/matiej/springsecstudy/user/controller/AdminController.java
@@ -0,0 +1,13 @@
+package com.matiej.springsecstudy.user.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AdminController {
+
+    @RequestMapping("/secured")
+    public String home() {
+        return "securedPage";
+    }
+}
