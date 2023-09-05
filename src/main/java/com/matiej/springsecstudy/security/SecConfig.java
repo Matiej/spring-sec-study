c033af6cc6d41acf95fde661d812204447ac4218
Maciej Wójcik <maciek.wojcik179@gmail.com>
2023-09-05 21:58:13 +0200

    finally I got when use ROLE_ prefix. Test in teplates and secured page.

--- src/main/java/com/matiej/springsecstudy/security/SecConfig.java
+++ src/main/java/com/matiej/springsecstudy/security/SecConfig.java
@@ -2,6 +2,7 @@ package com.matiej.springsecstudy.security;
 
 import com.matiej.springsecstudy.user.application.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -9,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -24,10 +26,11 @@ import javax.sql.DataSource;
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
-@EnableConfigurationProperties(DefaultAdmin.class)
+@EnableConfigurationProperties({DefaultAdmin.class, TestUser.class})
 public class SecConfig {
     private final UserService userService;
     private final DefaultAdmin defaultAdmin;
+    private final TestUser testUser;
     private final DataSource dataSource;
     @Value("${app.security.cookie-token.validity.seconds}")
     private int cookieTokenValidity;
@@ -39,6 +42,7 @@ public class SecConfig {
             "/styles/**",
             "/h2-console/**",
             "/h2-console*",
+            "/reg/login", "/reg/login/**", "/reg/login*",
             "/reg/signup",
             "/reg/signup*",
             "/reg/signup/**",
@@ -65,7 +69,7 @@ public class SecConfig {
     @Bean
     public AuthenticationProvider authenticationProvider() {
         DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
-        daoAuthenticationProvider.setUserDetailsService(new UserEntityDetailService(userService, defaultAdmin));
+        daoAuthenticationProvider.setUserDetailsService(new UserEntityDetailService(userService, defaultAdmin, testUser));
         daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
         daoAuthenticationProvider.setHideUserNotFoundExceptions(false);// these exception after that setting is not wrapped by BadCredentialsException anymore
         return daoAuthenticationProvider;
@@ -76,14 +80,22 @@ public class SecConfig {
         http.headers().frameOptions().sameOrigin(); //important for h2 console
         http.anonymous().disable()
                 .csrf(AbstractHttpConfigurer::disable)// impornat for h2 database console
+
+                //todo JUST FOR TEST TO CHECK expressions!!
+                /*.authorizeRequests()
+                .requestMatchers(PERMIT_ALL).permitAll()
+                .requestMatchers("/delete/**").hasAuthority("ADMIN")
+                .requestMatchers("/secured").access("hasRole('USER')")
+                .requestMatchers("/secured").access("hasAuthority('ROLE_USER')")
+                .anyRequest().authenticated().and()*/
+
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(PERMIT_ALL).permitAll()
-                        .requestMatchers("/delete/**").hasAuthority("ADMIN")
+                        .requestMatchers("/delete/**").hasAuthority("ADMIN") //todo here can access only ADMIN + ROLE_ADMIN
+                        .requestMatchers("/secured*", "/secured/**", "/secured").hasRole("ADMIN")//todo secured will access user that has ROLE_ADMIN  in DB
                         .anyRequest().authenticated())
-
                 .formLogin()
-                .loginPage("/reg/login")
-                .permitAll()
+                .loginPage("/reg/login").permitAll()
                 .loginProcessingUrl("/do-logging")
 
                 .and()
@@ -112,5 +124,4 @@ public class SecConfig {
         jdbcTokenRepository.setDataSource(dataSource);
         return jdbcTokenRepository;
     }
-
 }
--- src/main/java/com/matiej/springsecstudy/user/controller/AdminController.java
+++ src/main/java/com/matiej/springsecstudy/user/controller/AdminController.java
@@ -1,13 +1,24 @@
 package com.matiej.springsecstudy.user.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class AdminController {
 
+//    @RequestMapping("/secured")
+//    public ModelAndView getSecuredPage(@RequestBody BindingResult result) {
+//        if (result.hasErrors()) {
+//            return new ModelAndView("unauthorized", "formErrors", result.getAllErrors());
+//        }
+//        return new ModelAndView("securedPage");
+//    }
+
     @RequestMapping("/secured")
-    public String home() {
-        return "securedPage";
+    public String getSecuredPage() {
+          return "securedPage";
     }
 }
