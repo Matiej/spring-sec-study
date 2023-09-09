fbd4ef9f1fb2199390be74216c2e898a2e83f31c
Maciej Wójcik <maciek.wojcik179@gmail.com>
2023-09-09 18:27:02 +0200

    add unauthorized page and customDenied handled to customize messages

---
+++ src/main/java/com/matiej/springsecstudy/security/CustomAccessDeniedHandler.java
@@ -0,0 +1,55 @@
+package com.matiej.springsecstudy.security;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.apache.logging.log4j.util.Strings;
+import org.springframework.data.auditing.AuditingHandler;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.security.Principal;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+
+@Component
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+    private static final String ADMIN_URI = "/admin-settings";
+
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        String remoteUser = request.getRemoteUser();
+        String requestURI = request.getRequestURI();
+        Principal userPrincipal = request.getUserPrincipal();
+
+        String errorMessage = "You don't have permission to access this page.";
+
+        if (requestURI.contains("/admin-settings")) {
+            String principalRoles = getPrincipalRoles(userPrincipal);
+            String roles = Strings.isNotEmpty(principalRoles) ? principalRoles : "[no ROLES found]";
+            errorMessage = "User: " + remoteUser + " with ROLES: " + roles + " doesn't have permission to access this page!";
+        } else if (requestURI.contains("/securedIP")) {
+            errorMessage = "User: " + remoteUser + " with IP: " + request.getRemoteAddr() + " doesn't have permission to access this page!";
+        }
+
+        request.getSession().setAttribute("errorMessage", errorMessage);
+        response.sendRedirect(request.getContextPath() + "/unauthorized");
+
+    }
+
+    private String getPrincipalRoles(Principal principal) {
+        if (principal != null && principal instanceof Authentication) {
+            Authentication auth = (Authentication) principal;
+            return auth.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.joining(","));
+        }
+        return "";
+    }
+}
--- src/main/java/com/matiej/springsecstudy/user/controller/PathController.java
+++ src/main/java/com/matiej/springsecstudy/user/controller/PathController.java
@@ -1,12 +1,23 @@
 package com.matiej.springsecstudy.user.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller
 public class PathController {
     @RequestMapping("/")
     public String home() {
         return "redirect:/user";
+    }
+
+    @RequestMapping("/unauthorized")
+    public ModelAndView unauthorizedAccess(HttpServletRequest request) {
+        String errorMessage = (String) request.getSession().getAttribute("errorMessage");
+        request.getSession().removeAttribute("errorMessage");
+        return new ModelAndView("unauthorized", "errorMessage", errorMessage);
     }
 }
