fa52b6ed75141c544d4cdacc36c6f729a768a7a4
Maciej Wójcik <maciek.wojcik179@gmail.com>
2023-09-20 19:46:52 +0200

    add entity privilege and map with Role

---
+++ src/main/java/com/matiej/springsecstudy/user/domain/Privilege.java
@@ -0,0 +1,38 @@
+package com.matiej.springsecstudy.user.domain;
+
+import com.matiej.springsecstudy.global.jpa.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToMany;
+import lombok.*;
+
+import java.util.Objects;
+import java.util.Set;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+public class Privilege extends BaseEntity {
+    @Column(unique = true)
+    private String roleName;
+
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    @ManyToMany(mappedBy = "privileges")
+    private Set<Role> roles;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Privilege privilege = (Privilege) o;
+        return Objects.equals(roleName, privilege.roleName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), roleName);
+    }
+}
