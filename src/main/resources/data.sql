724136207e1691f21c7e32c4c897f3cbc8d76e94
Maciej Wójcik <maciek.wojcik179@gmail.com>
2023-09-26 22:07:19 +0200

    update inserts about privilages and jointable privilages_role

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,7 +5,21 @@ create table if not exists persistent_logins (
   last_used timestamp not null
 );
 
+INSERT IGNORE INTO privilege (name, id) VALUES ('READ_PRIVILEGE', 1);
+INSERT IGNORE INTO privilege (name, id) VALUES ('UPDATE_PRIVILEGE', 2);
+INSERT IGNORE INTO privilege (name, id) VALUES ('WRITE_PRIVILEGE', 3);
+INSERT IGNORE INTO privilege (name, id) VALUES ('DELETE_PRIVILEGE', 4);
+
 INSERT IGNORE INTO roles (id, role_name) VALUES
-(1, 'ROLE_USER'),
-(2, 'ROLE_ADMIN'),
-(3, 'ROLE_SECURED');
+(5, 'ROLE_USER'),
+(6, 'ROLE_ADMIN'),
+(7, 'ROLE_SECURED');
+
+insert IGNORE into roles_privileges (role_id, privilege_id) values (5, 1);
+insert IGNORE into roles_privileges (role_id, privilege_id) values (6, 1);
+insert IGNORE into roles_privileges (role_id, privilege_id) values (6, 2);
+insert IGNORE into roles_privileges (role_id, privilege_id) values (6, 3);
+insert IGNORE into roles_privileges (role_id, privilege_id) values (7, 1);
+insert IGNORE into roles_privileges (role_id, privilege_id) values (7, 2);
+insert IGNORE into roles_privileges (role_id, privilege_id) values (7, 3);
+insert IGNORE into roles_privileges (role_id, privilege_id) values (7, 4);
