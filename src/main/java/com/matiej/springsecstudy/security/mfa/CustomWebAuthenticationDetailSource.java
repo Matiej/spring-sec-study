dd41e803f5f84592f9588267cfebc8a9da4e80f4
Maciej Wójcik <maciek.wojcik179@gmail.com>
2023-11-25 18:41:10 +0100

    custom AuthDetails and CustomWebAuth implementation

---
+++ src/main/java/com/matiej/springsecstudy/security/mfa/CustomWebAuthenticationDetailSource.java
@@ -0,0 +1,14 @@
+package com.matiej.springsecstudy.security.mfa;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.authentication.AuthenticationDetailsSource;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomWebAuthenticationDetailSource implements AuthenticationDetailsSource<HttpServletRequest, WebAuthenticationDetails> {
+    @Override
+    public WebAuthenticationDetails buildDetails(HttpServletRequest context) {
+        return new CustomWebAuthDetails(context);
+    }
+}
